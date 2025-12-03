8f4569f9611f2f627300ed6c4300abfefbd76ed8
fernanqv <valvanuz.fernandez@unican.es>
2025-12-03 11:39:18 +0100

    First attemp in DelftBlue

--- copulas7.py
+++ copulas7.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ProcessPoolExecutor
 import numpy as np
 import pyvinecopulib as pv
 import time
@@ -5,45 +6,56 @@ from get_matrices import load_matrices_from_h5
 import csv
 import argparse
 
-def main(index):
+
+def fit_copula(index, vines_per_core, u, matrices7):
+    # index: starting index for this core.
+    # vines_per_core: number of vines to process per core
+
+    results = []    
+    first_vine=index*vines_per_core
+    for i in range(vines_per_core):
+        id=first_vine+i
+        #print([f.name for f in pv.BicopFamily])
+        controls = pv.FitControlsVinecop(family_set=pv.one_par, selection_criterion="aic", show_trace=False,parametric_method='mle')
+        cop = pv.Vinecop.from_data(u, matrix=matrices7[id], controls=controls)
+        aic = cop.aic()
+        results.append((int(id),int(cop.npars), aic))
+        # fit the sample to the structure wit specified controls
+        # get the AIC score for the matrix on the sample
+
+    return results
+
+    # Write to file after finishing the loop
+
+def main(index,cores,vines_per_core):
     data = np.loadtxt('input7.txt')
 
     # Transform copula data using the empirical distribution
     u = pv.to_pseudo_obs(data)   
     matrices7 = load_matrices_from_h5("matrices_nodes7.h5")
-
-    #Fit all the model
-    # measure time
-    # Iterate through all loaded matrices
-    #for i in range(5):
-    results=[]
-    
     start_time = time.time()
-    for i in range(100):
-        id=2000*index+i
-        #print([f.name for f in pv.BicopFamily])
-        controls = pv.FitControlsVinecop(family_set=pv.one_par, selection_criterion="aic", show_trace=False,parametric_method='mle')
-        cop = pv.Vinecop.from_data(u, matrix=matrices7[id], controls=controls)
-        aic = cop.aic()
-        results.append((id,int(cop.npars), aic))
-        # fit the sample to the structure wit specified controls
-        # get the AIC score for the matrix on the sample
-    print("Time taken for 100:", time.time() - start_time)
+    with ProcessPoolExecutor(max_workers=cores) as ex:
+        futures = [ex.submit(fit_copula, index*cores+i, vines_per_core, u, matrices7) for i in np.arange(cores)]
 
-    # Write to file after finishing the loop
-    output_file = f"salidas/results_run_{index}.csv"
-
-    with open(output_file, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(results)
+    print(f"Time taken for {cores} parallel cores running {vines_per_core} vines per core: "  , time.time() - start_time)    
+    output_file = f"salidas/results7_id{index}.csv"
+    with open(output_file, "w", newline="") as f_output_file:        
+        writer = csv.writer(f_output_file)
+        for f in futures:           
+            writer.writerows(f.result())
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process one matrix by id")
-    parser.add_argument("id", type=int, help="ID (0-based) of the matrix to process")
-    args = parser.parse_args()
-
-    # call main with the selected id (modify main to accept this argument)
-    main(args.id)
-    #main(1000)
+    # Change this variable to 0 to use command line arguments
+    internal=0
+    if internal==0:
+        parser = argparse.ArgumentParser(description="Process one matrix by id")
+        parser.add_argument("id", type=int, help="ID (0-based) of the matrix to process")
+        parser.add_argument("cores", type=int, help="Number of cores to use for processing")
+        parser.add_argument("vines_per_core", type=int, help="Number of vines to process per core")
+        args = parser.parse_args()
+        main(args.id, args.cores, args.vines_per_core)
+        print(f"Processed {args.id} using {args.cores} cores with {args.vines_per_core} vines per core.")
+    else:
+        main(5,16,100)
 
--- launchVine.sh
+++ launchVine.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name="vines"
-#SBATCH --partition=geocean
-#SBATCH --time=08:00:00
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --partition=compute
+#SBATCH --time=00:10:00
+#SBATCH --ntasks=16
+##SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2GB
-#SBATCH --array=0,221,222,225,448,449,450,476,576,577,580,684
+#SBATCH --mem-per-cpu=1024MB
+#SBATCH --array=3
+##SBATCH --array=0-4
 
 
 ##SBATCH --output=compiled.log
@@ -17,4 +19,6 @@
 # eval "$($MAMBA_EXE shell hook --shell bash --root-prefix $MAMBA_ROOT_PREFIX)"
 
 # Activate pytorch environment
-python copulas_T24.py $SLURM_ARRAY_TASK_ID 
+# Launch 4 jobs. Each uses 16 cores and runs 645120/16=40320
+
+python copulas7.py $SLURM_ARRAY_TASK_ID 16 4032
